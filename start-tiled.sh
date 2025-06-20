a6b9879d68461e9452a43a39d1e5fcb6e2dbf45c
Pete R Jemian <prjemian@gmail.com>
2025-06-20 18:34:17 -0500

    MNT find the correct base environment tools

--- start-tiled.sh
+++ start-tiled.sh
@@ -8,9 +8,10 @@ HOST=0.0.0.0  # access server by "localhost", hostname, or IP number
 # HOST="${HOSTNAME}"  # only access server by this exact name
 PORT=8020
 
-source ${CONDA_PREFIX}/etc/profile.d/conda.sh
-CONDA_ENV=tiled
-conda activate "${CONDA_ENV}"
+TILED_ENV=tiled
+CONDA_BASE=$(realpath $(dirname ${CONDA_EXE})/..)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate "${TILED_ENV}"
 
 
 # strace -fe openat,lstat \
